022f42d325c0b3c6201ea120173043af8c1cbc22
akwanmaroso <akwanmaroso@gmail.com>
2025-06-08 19:44:30 +0800

    wip

--- programs/dapps-campaign/src/lib.rs
+++ programs/dapps-campaign/src/lib.rs
@@ -6,11 +6,44 @@ declare_id!("9k1Z1sPcHRBNpU5rAcv1EfLA3eK6dN7CX21bv2Em6Zfg");
 pub mod dapps_campaign {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_campaign(
+        ctx: Context<CreateCampaign>,
+        name: String,
+        description: String,
+        target: u64,
+    ) -> Result<()> {
+        let campaign = &mut ctx.accounts.campaign;
+        campaign.owner = *ctx.accounts.signer.key;
+        campaign.name = name;
+        campaign.description = description;
+        campaign.target = target;
+        campaign.is_completed = false;
+        campaign.amount_donated = 0;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateCampaign<'info> {
+    #[account(init,payer = signer, space=8+Campaign::INIT_SPACE)]
+    pub campaign: Account<'info, Campaign>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Campaign {
+    pub owner: Pubkey,
+    #[max_len(50)]
+    pub name: String,
+    #[max_len(100)]
+    pub description: String,
+    pub target: u64,
+    pub amount_donated: u64,
+    pub is_completed: bool,
+}
--- tests/dapps-campaign.ts
+++ tests/dapps-campaign.ts
@@ -1,16 +1,33 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { Program, SystemProgram } from "@coral-xyz/anchor";
 import { DappsCampaign } from "../target/types/dapps_campaign";
+import { Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
 describe("dapps-campaign", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.dappsCampaign as Program<DappsCampaign>;
+  const campaignKeyPair = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const key = new Keypair();
+  const target = new anchor.BN(1 * LAMPORTS_PER_SOL);
+
+  it("Create a campaign", async () => {
+    const tx = await program.methods
+      .createCampaign("Save the Whales", "Help save ocean wildlife.", target)
+      .accounts({
+        signer: key.publicKey,
+        campaign: campaignKeyPair.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([campaignKeyPair])
+      .rpc();
+
+    const campaign = await program.account.campaign.fetch(
+      campaignKeyPair.publicKey
+    );
+
+    console.log("Campaign: ", campaign);
   });
 });
